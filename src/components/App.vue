3d1d4bbefd05e3c0fcf25ca3dd95c9d6b0fa8889
Jonathan Dann <nwjon122@gmail.coml>
2017-08-22 13:54:28 -0700

    initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,76 @@
+<template>
+    <div id="calendar">
+        <div v-for="week in weeks" class="calendar-week">
+            <calendar-day v-for="day in week" :day="day"></calendar-day>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import CalendarDay from "./CalendarDay.vue";
+
+    export default {
+        data() {
+            return  {
+                month: 5,
+                year: 2017
+            };
+        },
+        computed: {
+            days() {
+
+                //generate all days in the current month
+                let days = [];
+                let currentDay = this.$moment(`${this.year}-${this.month}-1`, "YYYY-M-D");
+
+                do {
+                    days.push(currentDay);
+                    currentDay = this.$moment(currentDay).add(1, "days");
+                } while ((currentDay.month() + 1) === this.month);
+
+                const SUNDAY = 0;
+                const MONDAY = 1;
+
+                //add previous days to start
+                currentDay = this.$moment(days[0]);
+
+                if (currentDay.day() !== MONDAY) {
+                    do {
+                        currentDay = this.$moment(currentDay).subtract(1, "days");
+                        days.unshift(currentDay);
+                    } while (currentDay.day() !== MONDAY);
+                }
+
+                //add following days to the end
+                currentDay = this.$moment(days[days.length - 1]);
+
+                if (currentDay.day() !== SUNDAY) {
+                    do {
+                        currentDay = this.$moment(currentDay).add(1, "days");
+                        days.push(currentDay);
+                    } while(currentDay.day() !== SUNDAY);
+                }
+
+                return days;
+            },
+            weeks() {
+                let weeks = [];
+                let week = [];
+
+                for (let day of this.days) {
+                    week.push(day);
+                    if (week.length === 7) {
+                        weeks.push(week);
+                        week = [];
+                    }
+                }
+                return weeks;
+            }
+        },
+        components: {
+            CalendarDay
+        }
+    }
+
+</script>
